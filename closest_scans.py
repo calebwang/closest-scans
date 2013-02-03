0465ba546d24d7f19c7e807718e7160d77f4651f
Caleb Wang <cw@berkeley.edu>
2013-02-03 12:11:23 -0800

    added get_closest

--- closest_scans.py
+++ closest_scans.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 def get_scans(data_dir, id, scantype):
     """FDG, FDG2, NIFDFDG2, PIB, COG_S#, NIFDPIB"""
+    if not data_dir:
+        data_dir = os.getcwd()
     globstr = os.path.join(data_dir, id, scantype.upper() + '*')
     files = glob(globstr)
     files.sort()
@@ -33,10 +35,22 @@ def find_closest(filelist, ref_date = datetime.now()):
             closest = (filename, time_diff)
     return closest
 
+def get_closest(data_dir, id, scan_type, ref_date = datetime.now()):
+    if not data_dir:
+        data_dir = os.getcwd()
+    scans = get_scans(data_dir, id, scan_type)
+    closest = find_closest(scans, ref_date)
+    return closest
+
 def parse_excel(filename):
     ef = pandas.ExcelFile(filename)
     df = ef.parse(ef.sheet_names[0])
     cols = df.columns
-    #verify correct column structure
+    #verify correct column structure here
     entries = df.to_records().tolist()
     return entries
+
+def process_excel(filename):
+    entries = parse_excel(filename)
+    for entry in entries:
+        find_closest
